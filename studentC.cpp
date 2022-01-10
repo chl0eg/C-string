3cfc94976c3af7b20103c253a968d2035bb046f3
LI Danlin <dli@bs406-0010-02.insa-lyon.fr>
2022-01-10 11:17:04 +0100

    afficher qqch avec destructor

--- studentC.cpp
+++ studentC.cpp
@@ -1,5 +1,13 @@
 #include "string.h"
+
 #include <iostream>
 
 ourString::~string(){
+  cout<< "string is being deleted" << endl;
 }
+
+/*test*/
+int main(){
+	string s;
+	std::cout << s.gettab() << std::endl;
+	}
