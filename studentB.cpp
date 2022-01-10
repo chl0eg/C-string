e8e39afd724f70c752b4e26d03adbbd839b250e7
GUISNET <chloe.guisnet@insa-lyon.fr>
2022-01-10 11:55:44 +0100

    ras

--- studentB.cpp
+++ studentB.cpp
@@ -1,51 +1,58 @@
 #include <iostream>
 #include "string.h"
 
-/*************** constructeur de ourString ***************/
+/*************** constructeur de string ***************/
 string::string (){ // ajout constructeur par défaut 
 	tab [0] = 'H';
 	tab [1] = 'e';
 	tab [2] = 'l';
 	tab [3] = 'l';
 	tab [4] = 'o';
+	len=5;
 	}
 
+
 	
-//string::string(char* s){ //constructeur str donné
-//	str= char[];
-//	for (int i; i<s.length(); i++){
-//		str[i]=s[i];
-//		}
-//	}
+string::string(char* s){ //constructeur str donné
+	int l=0;
+	int i = 0;
+	while (s[i]!='\0'){
+		i++;
+		l++;
+		} 
+	len=l;
+	
+	for (int i; i<l; i++){
+		tab[i]=s[i];
+		}
+	}
 
 /*************** accesseur ***************/
 char* string::gettab(){ //get tab
 	return this->tab;
 	}
 
+//char* string::getlength(){ //get length
+//	return this -> len;
+//	}
+
 
 /*************** fonction ***************/
 
-int string::length (){
-	int l=0;
-	int i = 0;
-	while (tab[i]!='\0'){
-		i++;
-		l++;
-		} 
-		
-	return (l);
-	}
+//int string::length (){
+//	return this -> len;
+//g++	}
 
-
+char*
 
 /*************** main test ***************/
 int main(){
 	string s;
 	std::cout << s.gettab() << std::endl;
-	std::cout << s.length() << std::endl;
-	//char my_tab[]="Bonjour";
-	//string s2(char my_tab[]);
+	//std::cout << s.length() << std::endl;
+	char my_tab[]="Bonjour";
+	string s2(my_tab);
+	std::cout << s2.print() << std::endl;
 	}
 
 
