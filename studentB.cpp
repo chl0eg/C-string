4e762f4f22924c43458dcbca41cfa345f104c6b2
GUISNET <chloe.guisnet@insa-lyon.fr>
2022-01-10 10:29:12 +0100

    constructeur par def fonctionne, ajout accesseur fonctionnel et test main

--- studentB.cpp
+++ studentB.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
-#include <ourString.h>
+#include "ourString.h"
 
 /*************** constructeur de ourString ***************/
-ourString::ourString (){ // ajout constructeur
-	tab [0] = 'H'
-	tab [1] = 'e'
-	tab [2] = 'l'
-	tab [3] = 'l'
-	tab [4] = 'o'
+ourString::ourString (){ // ajout constructeur par défaut 
+	tab [0] = 'H';
+	tab [1] = 'e';
+	tab [2] = 'l';
+	tab [3] = 'l';
+	tab [4] = 'o';
+	}
+
+/*************** accesseur ***************/
+char* ourString::gettab(){ //get tab
+	return this->tab;
 	}
 
 
+/*************** main test ***************/
 int main(){
-	ourString s
-	std::cout << s << std::endl;
-}
+	ourString s;
+	std::cout << s.gettab() << std::endl;
+	}
 
 
 
